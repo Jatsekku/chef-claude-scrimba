566578630a771696cdf941dde7aae772f94bb9bb
Jatsekku <jatsekku2@gmail.com>
2025-01-05 13:50:20 +0100

    feat(header): Add Header component

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from '../images/chef-claude-icon.png'
+
+export default function() {
+    return (
+        <header className='header'>
+            <img src={chefClaudeLogo}/>
+            <span>Chef Claude</span>
+        </header>
+    )
+}
