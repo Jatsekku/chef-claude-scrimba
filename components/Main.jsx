7a95437c2cb145c2bc829ca6234af593316fd90b
Jatsekku <jatsekku2@gmail.com>
2025-01-07 00:29:51 +0100

    feat(main): Handle ingredients logic

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
+
 export default function Main(){
+    const [ingredients, setIngredients] = useState([])
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
+    const ingredientsListItem = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
     return (
         <main className='main'>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type='text'
                     placeholder='e.g. oregano'
                     aria-label='Add ingredient'
+                    name='ingredient'
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItem}
+            </ul>
         </main>
     )
 }
